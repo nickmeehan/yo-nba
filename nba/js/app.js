3128de1aad66cf9f2bff9f63d37968caeb1ff4f8
Nick Meehan <nnmeehan@gmail.com>
2014-10-07 16:40:50 -0600

    Adding CNET to app, and making NBA specific names generic
    - on the client side we will check to see if the URL contains 'nba' or
      'cnet' and depending on which, a GET request will be made to a certain
      route on the server to retrieve the JSON
    
    - eventually, I would like to check to see if /nba and /cnet routes are
      even needed if vanity urls are being used, for example www.yonba.co would
      contain 'nba', and fulfill the client side logic

--- nba/js/app.js
+++ nba/js/app.js
@@ -1,13 +1,24 @@
-var nbadex = angular.module('nbadex', []);
+var subIndex = angular.module('subIndex', []);
+var currentUrl = $(location).attr('href')
+String.prototype.contains = function(subString) {
+  return this.indexOf(subString) != -1
+}
 
-nbadex.controller('DirectoryCtrl', [ '$scope',
+subIndex.controller('DirectoryCtrl', [ '$scope',
                                     '$filter',
                                     '$http',
   function($scope, $filter, $http) {
     $scope.services = [];
-    $http( {method:'GET', url:'./../nba/services.json'} )
-      .success(function(data,status) {
-        $scope.services = data
-      })
+    if (currentUrl.contains('nba')) {
+      $http( {method:'GET', url:'/nba/services.json'} )
+        .success(function(data,status) {
+          $scope.services = data
+        })
+    } else if (currentUrl.contains('cnet')) {
+      $http( {method:'GET', url:'/cnet/services.json'} )
+        .success(function(data,status) {
+          $scope.services = data
+        })
+    }
   }
 ])
--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 var express = require('express')
-  , mongo = require('mongodb')
-  , monk = require('monk')
   , path = require('path')
   , port = process.env.PORT || 8001
   , GoogleSpreadsheet = require("google-spreadsheet");
 
 var index = express.Router();
 
+function Service(data) {
+  this.name = data.name;
+  this.username = data.username;
+  this.sends_yo_when = data.saysyowhen;
+  this.url = data.url;
+}
+
 index.get('/nba/services.json', function(req, res) {
-
-  function NbaService(data) {
-    this.name = data.name;
-    this.username = data.username;
-    this.sends_yo_when = data.saysyowhen;
-    this.url = data.url;
-  }
-
   var teams = []
   var nbaSheet = new GoogleSpreadsheet('1CmdqaWY31EQ_NAgjg635fBIzL7h5gZuxQBCJ_uvNnXc');
   nbaSheet.getRows( 1, function(err, rowData){
     for (var i = 0; i < rowData.length; i++) {
-      teams.push(new NbaService(rowData[i]))
+      teams.push(new Service(rowData[i]))
+    }
+    res.send(teams)
+  })
+});
+
+index.get('/cnet/services.json', function(req, res) {
+  var teams = []
+  var cnetSheet = new GoogleSpreadsheet('1pFU5GMO_1VtiP4w5rEC9O-yM54qVIVNVsui3jFQRQjo');
+  cnetSheet.getRows( 1, function(err, rowData){
+    for (var i = 0; i < rowData.length; i++) {
+      teams.push(new Service(rowData[i]))
     }
     res.send(teams)
   })
@@ -31,4 +39,8 @@ index.get('/nba/services.json', function(req, res) {
 express()
   .use("/", index)
   .use("/", express.static(path.join(__dirname, 'nba')))
+  .use("/nba", index)
+  .use("/nba", express.static(path.join(__dirname, 'nba')))  
+  .use("/cnet", index)
+  .use("/cnet", express.static(path.join(__dirname, 'nba')))  
   .listen(port)
